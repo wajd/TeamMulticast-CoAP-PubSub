a1298d247a51cb0e84dc2f0064f394f5a8458384
Matias Carlander-Reuterfelt <matias@n140-p233.eduroam.kth.se>
2019-05-06 14:49:50 +0200

    topic object DIS+CRT

--- src/main/java/PubSub.java
+++ src/main/java/PubSub.java
@@ -10,39 +10,45 @@ import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class PubSub {
-    public static String[] discover(String host, int port, long timeout, String path){
-        CoapClient client = new CoapClient("coap", host, port,path);
+
+    /* Returns array of Topic objects */
+    public static Topic[] discover(String host, int port, long timeout, String path) {
+        CoapClient client = new CoapClient("coap", host, port, path);
         client.setTimeout(timeout);
         String content = client.get().getResponseText();
-        String[] topics = content.split(",");
-        for(int i = 0; i < topics.length; i++) {
-            System.out.println("FOUND TOPIC " + (i+1) + ":      " + topics[i]);
+        String[] topicS = content.split(",");
+        Topic[] topicT = new Topic[topicS.length];
+        for (int i = 0; i < topicS.length; i++) {
+            topicT[i] = new Topic(topicS[i]);
         }
-        return topics;
+        return topicT;
     }
 
-    public static void create(String host, int port, long timeout, String path, String name, int ct){
-        CoapClient client = new CoapClient("coap", host, port,path);
-        StringBuilder sb = new StringBuilder().append("<").append(name).append(">;ct=").append(ct);
-        String payload = sb.toString();
-        CoapResponse resp = client.post(payload,0);
+    /* Returns Confirmation Code */
+    public static void create(String host, int port, String path, Topic topic) {
+        CoapClient client = new CoapClient("coap", host, port, path);
+        String payload = topic.makeCreate();
+        CoapResponse resp = client.post(payload, 0);
         System.out.println(resp.isSuccess());
         System.out.println(resp.getResponseText());
     }
 
-    public static void publish(String host, int port, String path, String payload, int ct){
-        CoapClient client = new CoapClient("coap", host, port,path);
-        client.put(payload,ct);
+    /* Returns Confirmation Code */
+    public static void publish(String host, int port, String path, String payload, int ct) {
+        CoapClient client = new CoapClient("coap", host, port, path);
+        client.put(payload, ct);
     }
 
-    public static void read(String host, int port, String path){
-        CoapClient client = new CoapClient("coap", host, port,path);
+    /* Returns Content */
+    public static void read(String host, int port, String path) {
+        CoapClient client = new CoapClient("coap", host, port, path);
         String data = client.get().getResponseText();
         System.out.println(data);
     }
 
-    public static void remove(String host, int port, String path){
-        CoapClient client = new CoapClient("coap", host, port,path);
+    /* Returns Confirmation Code */
+    public static void remove(String host, int port, String path) {
+        CoapClient client = new CoapClient("coap", host, port, path);
         client.delete();
     }
     public static void subscribe(String host, int port, String path){
@@ -58,10 +64,8 @@ public class PubSub {
                 System.out.println("ERROR");
             }
         };
-
         client.observe(handler);
-
-        while (true);
+        while (true) ;
 
     }
     public static void fakeSubscribe(String host, int port, String path) throws InterruptedException {
