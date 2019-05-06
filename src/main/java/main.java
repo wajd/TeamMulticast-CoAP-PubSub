3f970dfcfac0747db2436733700a6bcba5cc5d03
wajd <wajdjtohme@gmail.com>
2019-05-06 09:34:53 +0200

    changed testing code in main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,33 +2,20 @@ public class main {
 
     public static void main(String[] args) {
 
-       /* PubSub.discover("127.0.0.1", 5683, 5000,"/.well-known/core");
+        String host = "127.0.0.1";
+        int port = 5683;
+        long timeout = 5000;
 
-        PubSub.create("127.0.0.1",5683,5000,"ps","topic1",40);
+        PubSub.discover(host,port, timeout, ".well-known/core");
 
-        PubSub.discover("127.0.0.1", 5683, 5000,"/.well-known/core");
+//        PubSub.create(host, port, timeout, "ps/", "topic1", 40);
+//        PubSub.create(host, port, timeout , "ps/topic1", "topic2", 0);
+//
+//        PubSub.discover(host,port, timeout, ".well-known/core");
+//
+//        PubSub.publish(host, port, "ps/topic1/topic2", "500", 0);
+//        PubSub.read(host, port, "ps/topic1/topic2");
 
-        PubSub.create("127.0.0.1",5683,5000,"ps/topic1","topic2",0);
-
-        PubSub.create("127.0.0.1",5683,5000,"ps/topic1","topic3",0);
-
-        PubSub.discover("127.0.0.1", 5683, 5000,"/.well-known/core");
-
-        PubSub.publish("127.0.0.1", 5683, "ps/topic1/topic2", "hello",0);
-
-        PubSub.read("127.0.0.1", 5683, "ps/topic1/topic2");
-
-        PubSub.remove("127.0.0.1", 5683, "ps/topic1/topic2");
-
-        PubSub.discover("127.0.0.1", 5683, 5000,"/.well-known/core"); */
-
-        /*PubSub.create("127.0.0.1",5683,5000,"ps","topic1",40);
-        PubSub.create("127.0.0.1",5683,5000,"ps/topic1","topic2",0); */
-
-       PubSub.discover("130.229.135.28",5683, 5000, ".well-known/core");
-        PubSub.read("130.229.135.28", 5683, "ps/topic1/topic2");
-       PubSub.subscribe("130.229.135.28", 5683, "ps/topic1/topic2");
-
-
+        PubSub.subscribe(host, port, "ps/topic1/topic2");
     }
 }
