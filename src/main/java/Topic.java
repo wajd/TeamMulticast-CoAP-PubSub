6dd82a3775be25792df2a6d71e6fc998cc201492
Matias Carlander-Reuterfelt <matias@n140-p233.eduroam.kth.se>
2019-05-06 14:49:50 +0200

    topic object DIS+CRT

--- src/main/java/Topic.java
+++ src/main/java/Topic.java
@@ -4,12 +4,7 @@ public class Topic {
     private String path;
     private int ct;
 
-    public Topic() {
-        name = "";
-        path = null;
-        ct = 40;
-    }
-
+    /* Takes a URI and extracts the name, path and ct from it */
     public Topic(String format) {
         String[] small = format.split(";");
         this.ct = Integer.parseInt(small[1].substring(small[1].indexOf('=') + 1));
@@ -21,6 +16,23 @@ public class Topic {
         this.name = name;
         this.path = path;
         this.ct = ct;
+    }
+
+    public Topic(String name, int ct) {
+        this.name = name;
+        this.ct = ct;
+    }
+
+    /* Takes name, path and ct and makes them into a universal URI */
+    public String makeURI(){
+        StringBuilder sb = new StringBuilder().append("<").append(getPath()).append(">;ct=").append(getCt());
+        return sb.toString();
+    }
+
+    /* Takes name and ct and makes them intp a URI for the CREATE command*/
+    public String makeCreate() {
+        StringBuilder sb = new StringBuilder().append("<").append(getName()).append(">;ct=").append(getCt());
+        return sb.toString();
     }
 
     public String getName() {
