76827d1a04fbb77e84f8b7c0ee503612f47efee3
wajd <wajdjtohme@gmail.com>
2019-05-08 09:07:49 +0200

    added centralized ip address change to demo/test in main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,8 +4,12 @@ public class main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        String host = "130.229.160.206";
+        int port = 5683;
+        long timeout = 5000;
+
         System.out.println("Discover topics: ");
-        Topic[] topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
+        Topic[] topics = PubSub.discover(host, port, timeout, "/.well-known/core");
 
         System.out.println();
         System.out.println("FOUND " + topics.length + " TOPICS:");
@@ -20,7 +24,7 @@ public class main {
 
 
         System.out.println("Create topic1 in ps: ");
-        CoAP.ResponseCode code = PubSub.create("127.0.0.1", 5683, "ps", new Topic("topic1", 40));
+        CoAP.ResponseCode code = PubSub.create(host, port, "ps", new Topic("topic1", 40));
         if (code == CoAP.ResponseCode.CREATED) {
             System.out.println("CREATED");
         }
@@ -28,13 +32,13 @@ public class main {
 
         System.out.println("Create topic2 and topic3 inside topic 1");
 
-        PubSub.discover(host,port, timeout, ".well-known/core");
+        PubSub.create(host, port, "ps/topic1", new Topic("topic2", 0));
 
-        PubSub.create("127.0.0.1", 5683, "ps/topic1", new Topic("topic3", 0));
+        PubSub.create(host, port, "ps/topic1", new Topic("topic3", 0));
 
 
         System.out.println("Discover: ");
-        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
+        topics = PubSub.discover(host, port, timeout,"/.well-known/core");
 
         System.out.println();
         System.out.println("FOUND " + topics.length + " TOPICS:");
@@ -49,7 +53,7 @@ public class main {
 
 
         System.out.println("We publish a message: hello");
-        CoAP.ResponseCode resp = PubSub.publish("127.0.0.1", 5683, topics[0], "hello");
+        CoAP.ResponseCode resp = PubSub.publish(host, port, topics[0], "hello");
         System.out.println(resp.toString());
         if (resp == CoAP.ResponseCode.CHANGED) {
             System.out.println("CHANGED");
@@ -57,19 +61,19 @@ public class main {
         System.out.println();
 
         System.out.println("We read a message from the topic: ");
-        String s = PubSub.read("127.0.0.1", 5683, topics[0]);
+        String s = PubSub.read(host, port, topics[0]);
         System.out.println(s);
         System.out.println();
 
         System.out.println("We delete topic2: ");
-        CoAP.ResponseCode r = PubSub.remove("127.0.0.1", 5683, topics[0]);
+        CoAP.ResponseCode r = PubSub.remove(host, port, topics[0]);
         if (r == CoAP.ResponseCode.DELETED) {
             System.out.println("REMOVED");
         }
         System.out.println();
 
         System.out.println("We discover again:");
-        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
+        topics = PubSub.discover(host, port, timeout, "/.well-known/core");
         System.out.println("FOUND " + topics.length + " TOPICS:");
         for (int i = 0; i < topics.length; i++) {
             System.out.println();
