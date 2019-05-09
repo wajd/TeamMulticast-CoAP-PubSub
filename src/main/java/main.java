05274894c218e89948b793832ea535f0e438a106
Matias Carlander-Reuterfelt <matias@n144-p121.eduroam.kth.se>
2019-05-09 13:24:11 +0200

    added fakesubscribe, minor changes, demo updated

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -32,52 +32,52 @@ public class main {
         PubSub.create("127.0.0.1", 5683, topics[0], topic2);
         PubSub.create("127.0.0.1", 5683, topics[0], topic3);
 
-        PubSub.fakeSubscribe("127.0.0.1", 5683, "ps/topic1/topic2");
+//        PubSub.fakeSubscribe("127.0.0.1", 5683, "ps/topic1/topic2");
 
-//        System.out.println("Discover: ");
-//        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
-//
-//        System.out.println();
-//        System.out.println("FOUND " + topics.length + " TOPICS:");
-//        for (int i = 0; i < topics.length; i++) {
-//            System.out.println();
-//            System.out.println(topics[i].toString());
-//            System.out.println("Name:           " + topics[i].getName());
-//            System.out.println("Path:           " + topics[i].getPathString());
-//            System.out.println("Content type:   " + topics[i].getCt());
-//        }
-//        System.out.println();
-//
-//
-//        System.out.println("We publish a message: hello");
-//        CoAP.ResponseCode resp = PubSub.publish("127.0.0.1", 5683, topics[0], "hello");
-//        System.out.println(resp.toString());
-//        if (resp == CoAP.ResponseCode.CHANGED) {
-//            System.out.println("CHANGED");
-//        }
-//        System.out.println();
-//
-//        System.out.println("We read a message from the topic: ");
-//        String s = PubSub.read("127.0.0.1", 5683, topics[0]);
-//        System.out.println(s);
-//        System.out.println();
-//
-//        System.out.println("We delete " + topics[0].getName() + ": ");
-//        CoAP.ResponseCode r = PubSub.remove("127.0.0.1", 5683, topics[0]);
-//        if (r == CoAP.ResponseCode.DELETED) {
-//            System.out.println("REMOVED");
-//        }
-//        System.out.println();
-//
-//        System.out.println("We discover again:");
-//        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
-//        System.out.println("FOUND " + topics.length + " TOPICS:");
-//        for (int i = 0; i < topics.length; i++) {
-//            System.out.println();
-//            System.out.println(topics[i].toString());
-//            System.out.println("Name:           " + topics[i].getName());
-//            System.out.println("Path:           " + topics[i].getPathString());
-//            System.out.println("Content type:   " + topics[i].getCt());
-//        }
+        System.out.println("Discover: ");
+        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
+
+        System.out.println();
+        System.out.println("FOUND " + topics.length + " TOPICS:");
+        for (int i = 0; i < topics.length; i++) {
+            System.out.println();
+            System.out.println(topics[i].toString());
+            System.out.println("Name:           " + topics[i].getName());
+            System.out.println("Path:           " + topics[i].getPathString());
+            System.out.println("Content type:   " + topics[i].getCt());
+        }
+        System.out.println();
+
+
+        System.out.println("We publish a message: hello");
+        CoAP.ResponseCode resp = PubSub.publish("127.0.0.1", 5683, topics[0], "hello");
+        System.out.println(resp.toString());
+        if (resp == CoAP.ResponseCode.CHANGED) {
+            System.out.println("CHANGED");
+        }
+        System.out.println();
+
+        System.out.println("We read a message from the topic: ");
+        String s = PubSub.read("127.0.0.1", 5683, topics[0]);
+        System.out.println(s);
+        System.out.println();
+
+        System.out.println("We delete " + topics[0].getName() + ": ");
+        CoAP.ResponseCode r = PubSub.remove("127.0.0.1", 5683, topics[0]);
+        if (r == CoAP.ResponseCode.DELETED) {
+            System.out.println("REMOVED");
+        }
+        System.out.println();
+
+        System.out.println("We discover again:");
+        topics = PubSub.discover("127.0.0.1", 5683, 5000, "/.well-known/core");
+        System.out.println("FOUND " + topics.length + " TOPICS:");
+        for (int i = 0; i < topics.length; i++) {
+            System.out.println();
+            System.out.println(topics[i].toString());
+            System.out.println("Name:           " + topics[i].getName());
+            System.out.println("Path:           " + topics[i].getPathString());
+            System.out.println("Content type:   " + topics[i].getCt());
+        }
     }
 }
