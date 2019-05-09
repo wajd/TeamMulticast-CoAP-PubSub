53ba23a532feff9813d7b26b4a9ccb90500d3387
matiascr <49898779+matiascr@users.noreply.github.com>
2019-05-09 11:01:30 +0200

    pubsub using new topic.java

--- src/main/java/PubSub.java
+++ src/main/java/PubSub.java
@@ -3,10 +3,6 @@ import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.CoAP;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.concurrent.TimeUnit;
-
 
 public class PubSub {
 
@@ -23,14 +19,38 @@ public class PubSub {
         return topicT;
     }
 
-    /* Returns Confirmation Code */
-    public static CoAP.ResponseCode create(String host, int port, String path, Topic topic) {
-        CoapClient client = new CoapClient("coap", host, port, path);
+//    /* Returns Confirmation Code */
+//    public static CoAP.ResponseCode create(String host, int port, String path, Topic topic) {
+//        CoapClient client = new CoapClient("coap", host, port, path);
+//        String payload = topic.makeCreate();
+//        CoapResponse resp = client.post(payload, 0);
+//
+//        return CoAP.ResponseCode.valueOf(resp.getCode().value);
+//    }
+
+
+    /* ------------------------------ */
+
+    public static CoAP.ResponseCode create(String host, int port, Topic topic) {
+        CoapClient client = new CoapClient("coap", host, port, "ps");
         String payload = topic.makeCreate();
         CoapResponse resp = client.post(payload, 0);
 
         return CoAP.ResponseCode.valueOf(resp.getCode().value);
     }
+
+    public static CoAP.ResponseCode create(String host, int port, Topic parent, Topic child) {
+        CoapClient client = new CoapClient("coap", host, port, parent.getPath());
+        String payload = child.makeCreate();
+        CoapResponse resp = client.post(payload, 0);
+
+        child.setPath(parent);
+
+        return CoAP.ResponseCode.valueOf(resp.getCode().value);
+    }
+
+    /* ------------------------------ */
+
 
     /* Returns Confirmation Code */
     public static CoAP.ResponseCode publish(String host, int port, Topic topic, String payload) {
@@ -73,26 +93,5 @@ public class PubSub {
         };
         client.observe(handler);
         while (true) ;
-    }
-
-    public static void fakeSubscribe(String host, int port, String path) throws InterruptedException {
-
-        System.out.println("Fake Subscribe");
-
-        String newData, oldData=null;
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-
-        while(true) {
-            TimeUnit.SECONDS.sleep(5);
-
-            CoapClient client = new CoapClient("coap", host, port, path);
-
-            newData = client.get().getResponseText();
-            if(!newData.equals(oldData)){
-                System.out.println();
-                System.out.println(sdf.format(cal.getTime())+": "+newData);
-            }
-        }
     }
 }
