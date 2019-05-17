429cda75d297d858fbf5c44df776c3f123977a10
wajd <wajdjtohme@gmail.com>
2019-05-17 14:22:37 +0200

    Created converter class

---
+++ src/main/java/Converter.java
@@ -0,0 +1,103 @@
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.WebLink;
+import org.eclipse.californium.core.coap.LinkFormat;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class Converter {
+
+    /*helper functions for  CoapResponse */
+
+    public static Set<WebLink> getWebLinks(CoapResponse response){
+        return LinkFormat.parse(response.getResponseText());
+    }
+
+    /*helper functions for Set<WebLink>*/
+
+    public static WebLink[] getArray(Set<WebLink> webLinks) {
+        return (WebLink[]) webLinks.toArray();
+    }
+
+    public static Set<WebLink> extractAllTopics(Set<WebLink> webLinks) {
+        Set<WebLink> topics = new ConcurrentSkipListSet();
+        WebLink p;
+        for(WebLink w: webLinks) {
+            p = w;
+            while (!cleanUri(p.getURI()).equals("ps")) {
+                if (!topics.contains(p))
+                    topics.add(p);
+                p = getParent(p);
+            }
+        }
+        return topics;
+    }
+
+    /*helper functions for WebLink*/
+
+    public static WebLink makeWebLink(String uri, int ct) {
+        WebLink webLink = new WebLink(cleanUri(uri));
+        setCT(webLink, ct);
+        return webLink;
+    }
+
+    public static int getCT(WebLink webLink) {
+        return Integer.parseInt(webLink.getAttributes().getContentTypes().get(0));
+    }
+
+    public static void setCT(WebLink webLink, int ct) {
+        webLink.getAttributes().clearContentType();
+        webLink.getAttributes().addContentType(ct);
+    }
+
+    public static String getCTString (int ct) {
+        return MediaTypeRegistry.toString(ct);
+    }
+
+    public static String getName (WebLink webLink) {
+        return getName(webLink.getURI());
+    }
+
+    public static String getParentUri (WebLink webLink) {
+        return getParentUri(webLink.getURI());
+    }
+
+    public static WebLink getParent (WebLink webLink) {
+        return makeWebLink(getParentUri(webLink), 40);
+    }
+
+    public static Set<WebLink> getSubTopics(WebLink webLink, Set<WebLink> webLinks) {
+        return getSubTopics(webLink.getURI(), webLinks);
+    }
+
+    /*helper functions for uri*/
+
+    public static String getName (String uri) {
+        return cleanUri(uri).substring(uri.lastIndexOf('/')+1);
+    }
+
+    public static String getParentUri (String uri) {
+        return cleanUri(uri).substring(0, uri.lastIndexOf('/'));
+    }
+
+    public static String cleanUri (String uri) {
+        if(uri.startsWith("/"))
+            uri = uri.substring(1);
+        if (uri.endsWith("/"))
+            uri = uri.substring(0, uri.length()-1);
+        return uri;
+    }
+
+    public static Set<WebLink> getSubTopics(String uri, Set<WebLink> webLinks) {
+        uri = cleanUri(uri);
+        Set<WebLink> subtopics = new ConcurrentSkipListSet();
+        for (WebLink w: webLinks) {
+            if(cleanUri(w.getURI()).contains(uri) && !cleanUri(w.getURI()).equals(uri)) {
+                subtopics.add(w);
+            }
+        }
+        return subtopics;
+    }
+
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,130 @@
-import org.apache.log4j.BasicConfigurator;
-import org.eclipse.californium.core.CoapHandler;
-import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.WebLink;
+
+import java.io.IOException;
+import java.util.Set;
 
 public class main {
 
     public static void main(String[] args) throws  RuntimeException{
 
-        String host = "127.0.0.1";
+        String host = "130.229.148.241";
 //        BasicConfigurator.configure();//for logger
 
         /*constructor*/
         PubSub my = new PubSub(host);
 
+//        //Discover rt core
+//        System.out.println("Discover core.ps");
+//        System.out.println(my.discover("rt=core.ps").getResponseText());
+
+        //Create
+        System.out.println("Create topics");
+        System.out.println(my.create("ps", "topic1", 40).getResponseText());
+        System.out.println(my.create("ps", "topic2", 0).getResponseText());
+        System.out.println(my.create("ps", "topic3", 40).getResponseText());
+        System.out.println(my.create("ps/topic1", "topic4", 0).getResponseText());
+        System.out.println(my.create("ps/topic1","topic5", 40).getResponseText());
+
+//        //Discover with query
+//        System.out.println("Discover w query ct=40");
+//        System.out.println(Converter.getWebLinks(my.discover("ct=40")));
+
+        //Discover
+        System.out.println("Discover");
+
+        Set<WebLink> links = Converter.getWebLinks(my.discover());
+        System.out.println(links.toString());
+
+//        System.out.println("weblinks getarray");
+//        WebLink[] weblinkArray = Converter.getArray(links);
+//
+//        for(WebLink w: weblinkArray)
+//            System.out.println(w.toString());
+
+        System.out.println("Extract all topics");
+        Set<WebLink> allWebLinks = Converter.extractAllTopics(links);
+        for (WebLink w: allWebLinks)
+            System.out.println(w.toString());
+
+        System.out.println("Make a weblink");
+        WebLink webLink = Converter.makeWebLink("ps/topic1", 0);
+        System.out.println(webLink.toString());
+        System.out.println(webLink.getURI());
+        System.out.println(Converter.getCT(webLink));
+        System.out.println(Converter.getCTString(Converter.getCT(webLink)));
+        System.out.println("change CT");
+        Converter.setCT(webLink, 40);
+        System.out.println(Converter.getCT(webLink));
+        System.out.println("get name");
+        System.out.println(Converter.getName(webLink));
+        System.out.println("get parent uri");
+        System.out.println(Converter.getParentUri(webLink));
+        System.out.println("get parent weblink");
+        System.out.println(Converter.getParent(webLink).toString());
+
+        System.out.println("get Sub topics of topic1");
+        Set<WebLink> subtopics = Converter.getSubTopics(webLink, allWebLinks);
+        for (WebLink w: subtopics)
+            System.out.println(w.toString());
+
+
+//        //Publish
+//        System.out.println("Publish");
+//        System.out.println(my.publish("ps/topic3", "Hello", 0).getCode().name());
+//
+//        //Read
+//        System.out.println("Read");
+//        System.out.println(my.read("ps/topic3").getResponseText());
+//
+//        //Remove
+//        System.out.println("Remove");
+//        System.out.println(my.remove("ps/topic1/topic5").getCode().name());
+//
+//        /*Subscribe/Unsub*/
+//
+//        //Create handler
+//        CoapHandler handler = new CoapHandler() {
+//            @Override
+//            public void onLoad(CoapResponse coapResponse) {
+//                System.out.println("topic4: " + coapResponse.getResponseText());
+//            }
+//
+//            @Override
+//            public void onError() {
+//                System.out.println("ERROR");
+//            }
+//        };
+//
+//        //subscription constructor
+//        PubSub.Subscription subscription = my.new Subscription("ps/topic1/topic4", handler);
+//        //subscribe
+//        subscription.subscribe();
+//
+//        //wait 15 seconds
+//        Thread.sleep(15000);
+//
+//        //unsubscribe
+//        subscription.unsubscribe();
+//
+//        subscription.setHandler(new CoapHandler() {
+//            @Override
+//            public void onLoad(CoapResponse coapResponse) {
+//                System.out.println("Different handler: " + coapResponse.getResponseText());
+//            }
+//
+//            @Override
+//            public void onError() {
+//
+//            }
+//        });
+//
+//        //subscribe
+//        subscription.subscribe();
+//
+//        //wait 15 seconds
+//        Thread.sleep(15000);
+//
+//        //unsubscribe
+//        subscription.unsubscribe();
     }
 }
