c9b276a93ee2e87dcaafcd6ad2711b4b04c70600
matias <carlander.matias@gmail.com>
2019-05-20 08:15:39 +0200

    finalized topic

--- src/main/java/Topic.java
+++ src/main/java/Topic.java
@@ -1,7 +1,7 @@
 import org.eclipse.californium.core.WebLink;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Set;
 
 public class Topic {
 
@@ -10,7 +10,7 @@ public class Topic {
     int ct;
 
     public Topic(WebLink wl) {
-        this.path = wl.getURI().split("(/)");
+        this.path = wl.getURI().substring(1).split("(/)");
         this.name = this.path[this.path.length - 1];
         int corchete = wl.toString().indexOf('[');
 
@@ -21,35 +21,53 @@ public class Topic {
             StringBuilder sb = new StringBuilder().append(wl.toString().charAt(corchete + 1));
             this.ct = Integer.parseInt(sb.toString());
         }
-
-
     }
 
-    public Topic(String[] path, int ct){
+    public Topic(String[] path, int ct) {
         this.path = path;
-        this.name = path[path.length -1];
+        this.name = path[path.length - 1];
         this.ct = ct;
     }
 
-    public ArrayList<Topic> getTopics(Topic topic, ArrayList<Topic> at){
+    public static void makeArrayList(Set<WebLink> swl, ArrayList<Topic> at) {
+        for (WebLink wl : swl) {
+            Topic t = new Topic(wl);
+            t.getTopics(t, at);
+        }
+    }
 
-        at.add(topic);
+    public void getTopics(Topic topic, ArrayList<Topic> at) {
 
+        if (topic == null) {
+            return;
+        }
+        boolean flag = false;
+        for (Topic x :
+                at) {
+            if (topic.equals(x)) {
+                flag = true;
+            }
+        }
+        if (!flag) {
+            at.add(topic);
+        }
         getTopics(topic.getParent(), at);
-
-        return at;
     }
 
     public String toString() {
-        return "name:  " + this.name + "  path: " + this.path + "  ct: " + this.ct;
+        return "name: " + this.name + "\n   |path: " + this.getPathString() + "\n       |ct: " + this.ct;
     }
 
-    public Topic getParent(){
-        ArrayList<String> x = new ArrayList<String>(Arrays.asList(this.path));
-        x.remove(x.size() -1);
-        Topic parent = new Topic((String[]) x.toArray(), 40);
-
-                return parent;
+    public Topic getParent() {
+        String[] ps = new String[this.path.length - 1];
+        for (int i = 0; i < this.path.length - 1; i++) {
+            ps[i] = this.path[i];
+        }
+        if (ps.length != 0) {
+            Topic parent = new Topic(ps, 40);
+            return parent;
+        }
+        return null;
     }
 
     public int getCt() {
@@ -62,5 +80,18 @@ public class Topic {
 
     public String[] getPath() {
         return path;
+    }
+
+    public String getPathString() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : this.getPath()) {
+            sb.append(s).append("/");
+        }
+        String p = sb.toString();
+        return p = p.substring(0, p.length() - 1);
+    }
+
+    public boolean equals(Topic topic) {
+        return (this.getName().equals(topic.getName()) && this.getPathString().equals(topic.getPathString()));
     }
 }
