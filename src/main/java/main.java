1da534604d4aa7a32f68a35d9c742cb45db9cd33
wajd <wajdjtohme@gmail.com>
2019-05-20 13:25:40 +0200

    fixed makeweblink

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,7 +9,7 @@ public class main {
 
     public static void main(String[] args) throws  RuntimeException{
 
-        String host = "";
+        String host = "130.229.141.239";
 //        BasicConfigurator.configure();//for logger
 
         /*constructor*/
@@ -34,99 +34,102 @@ public class main {
         //Discover
         System.out.println("Discover");
 
-        Set<WebLink> links = Converter.getWebLinks(my.discover());
+        Set<WebLink> links = Converter.getAllWebLinks(my.discover());
         System.out.println(links.toString());
 
-        System.out.println("weblinks getarray");
-        WebLink[] weblinkArray = Converter.getArray(links);
+        for(WebLink w: links)
+            System.out.println(w.getURI().toString());
 
-        for(int i=0; i<weblinkArray.length;i++){
-            System.out.println(weblinkArray[i]);
-        }
-
-        System.out.println("Extract all topics");
-        Set<WebLink> allWebLinks = Converter.extractAllWebLinks(links);
-        for (WebLink w: allWebLinks)
-            System.out.println(w.toString());
-
-        System.out.println("Make a weblink");
-        WebLink webLink = Converter.makeWebLink("ps/topic1", 0);
-        System.out.println(webLink.toString());
-        System.out.println(webLink.getURI());
-        System.out.println(Converter.getCT(webLink));
-        System.out.println(Converter.getCTString(Converter.getCT(webLink)));
-        System.out.println("change CT");
-        Converter.setCT(webLink, 40);
-        System.out.println(Converter.getCT(webLink));
-        System.out.println("get name");
-        System.out.println(Converter.getName(webLink));
-        System.out.println("get parent uri");
-        System.out.println(Converter.getParentUri(webLink));
-        System.out.println("get parent weblink");
-        System.out.println(Converter.getParent(webLink).toString());
-
-        System.out.println("get Sub topics of topic1");
-        Set<WebLink> subtopics = Converter.getSubTopics(webLink, allWebLinks);
-        for (WebLink w: subtopics)
-            System.out.println(w.toString());
-
-        //Publish
-        System.out.println("Publish");
-        System.out.println(my.publish("ps/topic3", "Hello", 0).getCode().name());
-
-        //Read
-        System.out.println("Read");
-        System.out.println(my.read("ps/topic3").getResponseText());
-
-        //Remove
-        System.out.println("Remove");
-        System.out.println(my.remove("ps/topic1/topic5").getCode().name());
-
-        /*Subscribe/Unsub*/
-
-        //Create handler
-        CoapHandler handler = new CoapHandler() {
-            @Override
-            public void onLoad(CoapResponse coapResponse) {
-                System.out.println("topic4: " + coapResponse.getResponseText());
-            }
-
-            @Override
-            public void onError() {
-                System.out.println("ERROR");
-            }
-        };
-
-        //subscription constructor
-        PubSub.Subscription subscription = my.new Subscription("ps/topic1/topic4", handler);
-        //subscribe
-        subscription.subscribe();
-
-        //wait 15 seconds
-        Thread.sleep(15000);
-
-        //unsubscribe
-        subscription.unsubscribe();
-
-        subscription.setHandler(new CoapHandler() {
-            @Override
-            public void onLoad(CoapResponse coapResponse) {
-                System.out.println("Different handler: " + coapResponse.getResponseText());
-            }
-
-            @Override
-            public void onError() {
-
-            }
-        });
-
-        //subscribe
-        subscription.subscribe();
-
-        //wait 15 seconds
-        Thread.sleep(15000);
-
-        //unsubscribe
-        subscription.unsubscribe();
+//        System.out.println("weblinks getarray");
+//        WebLink[] weblinkArray = Converter.getArray(links);
+//
+//        for(int i=0; i<weblinkArray.length;i++){
+//            System.out.println(weblinkArray[i]);
+//        }
+//
+//        System.out.println("Extract all topics");
+//        Set<WebLink> allWebLinks = Converter.extractAllWebLinks(links);
+//        for (WebLink w: allWebLinks)
+//            System.out.println(w.toString());
+//
+//        System.out.println("Make a weblink");
+//        WebLink webLink = Converter.makeWebLink("ps/topic1", 0);
+//        System.out.println(webLink.toString());
+//        System.out.println(webLink.getURI());
+//        System.out.println(Converter.getCT(webLink));
+//        System.out.println(Converter.getCTString(Converter.getCT(webLink)));
+//        System.out.println("change CT");
+//        Converter.setCT(webLink, 40);
+//        System.out.println(Converter.getCT(webLink));
+//        System.out.println("get name");
+//        System.out.println(Converter.getName(webLink));
+//        System.out.println("get parent uri");
+//        System.out.println(Converter.getParentUri(webLink));
+//        System.out.println("get parent weblink");
+//        System.out.println(Converter.getParent(webLink).toString());
+//
+//        System.out.println("get Sub topics of topic1");
+//        Set<WebLink> subtopics = Converter.getSubTopics(webLink, allWebLinks);
+//        for (WebLink w: subtopics)
+//            System.out.println(w.toString());
+//
+//        //Publish
+//        System.out.println("Publish");
+//        System.out.println(my.publish("ps/topic3", "Hello", 0).getCode().name());
+//
+//        //Read
+//        System.out.println("Read");
+//        System.out.println(my.read("ps/topic3").getResponseText());
+//
+//        //Remove
+//        System.out.println("Remove");
+//        System.out.println(my.remove("ps/topic1/topic5").getCode().name());
+//
+//        /*Subscribe/Unsub*/
+//
+//        //Create handler
+//        CoapHandler handler = new CoapHandler() {
+//            @Override
+//            public void onLoad(CoapResponse coapResponse) {
+//                System.out.println("topic4: " + coapResponse.getResponseText());
+//            }
+//
+//            @Override
+//            public void onError() {
+//                System.out.println("ERROR");
+//            }
+//        };
+//
+//        //subscription constructor
+//        PubSub.Subscription subscription = my.new Subscription("ps/topic1/topic4", handler);
+//        //subscribe
+//        subscription.subscribe();
+//
+//        //wait 15 seconds
+//        Thread.sleep(15000);
+//
+//        //unsubscribe
+//        subscription.unsubscribe();
+//
+//        subscription.setHandler(new CoapHandler() {
+//            @Override
+//            public void onLoad(CoapResponse coapResponse) {
+//                System.out.println("Different handler: " + coapResponse.getResponseText());
+//            }
+//
+//            @Override
+//            public void onError() {
+//
+//            }
+//        });
+//
+//        //subscribe
+//        subscription.subscribe();
+//
+//        //wait 15 seconds
+//        Thread.sleep(15000);
+//
+//        //unsubscribe
+//        subscription.unsubscribe();
     }
 }
